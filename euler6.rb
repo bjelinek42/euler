ebe18698461f5d0572ca7a489e51fc2fa260f476
ben <bjelinek42@gmail.com>
2022-03-08 14:56:09 -0700

    euler 6

---
+++ euler6.rb
@@ -0,0 +1,24 @@
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+num = 1
+total = 0
+while num <= 100
+  total += (num**2)
+  num += 1
+end
+
+p total
+# 338350
+
+total2 = 0
+num2 = 1
+while num2 <= 100
+  total2 = total2 + num2
+  num2 += 1
+end
+total2 = total2**2
+
+p total2
+
+answer = total2 - total
+p answer

9ed2f29fc397cfe64ff9220803961cdab0bc901e
ben <bjelinek42@gmail.com>
2022-03-11 09:47:42 -0700

    sum of 5s and subtract factors of 3 and 5)

--- euler1.rb
+++ euler1.rb
@@ -1,14 +1,42 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-# num = 3
-# total = 0
-# while num <= 1000
-#   if (num % 3) == 0
-#     p num
-#     total = total + num
-#     p total
-#   end
-#   num += 1
-# end
-# p total
+num = 3
+total = 0
+while num < 1000
+  if (num % 3) == 0
+    p num
+    total = total + num
+    p total
+  end
+  num += 1
+end
+p total
 
+num = 5
+total2 = 0
+while num < 1000
+  if (num % 5) == 0
+    p num
+    total2 = total2 + num
+    p total2
+  end
+  num += 1
+end
+p total2
+
+num = 0
+total3 = 0
+while num < 1000
+  if (num % 5) == 0 && (num % 3) == 0
+    p num
+    total3 = total3 + num
+    p total3
+  end
+  num += 1
+end
+p total3
+
+
+
+answer = total + total2 - total3
+p answer

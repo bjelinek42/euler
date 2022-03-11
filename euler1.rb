2267f10dcdb2fa1e53f333e6631604f6c23cd214
ben <bjelinek42@gmail.com>
2022-03-11 09:37:44 -0700

    sum of 3s

---
+++ euler1.rb
@@ -0,0 +1,14 @@
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# num = 3
+# total = 0
+# while num <= 1000
+#   if (num % 3) == 0
+#     p num
+#     total = total + num
+#     p total
+#   end
+#   num += 1
+# end
+# p total
+
